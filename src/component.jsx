4103ef6197fe6dabe43e26ce9a5d9d9c4514fbde
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-20 07:16:11 +0200

    React

--- src/component.jsx
+++ src/component.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 const MyFunc = () => {
-    const [name, setName] = useState(""); // Initialize with an empty string
-    const [age, setAge] = useState(0); // Initialize age with 0
+    const [name, setName] = useState(""); 
+    const [age, setAge] = useState(0); 
 
     const updateName = () => {
         setName("Iraguha");
@@ -21,7 +21,7 @@ const MyFunc = () => {
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
             <p>Is isEmployeed:{isEmployeed ? "Yes":"No"} </p>
-            <p>Age: {age}</p> {/* Fixed the second name display to age display */}
+            <p>Age: {age}</p> 
             <button onClick={increment}>Increment Age</button>
             <button onClick={decrement}>Decrement Age</button>
             <button onClick={toggleEmployeestatus}>Decrement Age</button>
---
+++ src/message.jsx
@@ -0,0 +1,4 @@
+const Message=()=>{
+return <h2>Welldone brh</h2>
+}
+export default Message;
