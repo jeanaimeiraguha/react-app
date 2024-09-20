1b5347fd80e75484dd2ac953d3589378eb1d9267
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-20 07:27:28 +0200

    React

--- src/component.jsx
+++ src/component.jsx
@@ -4,10 +4,14 @@ const MyFunc = () => {
     const [name, setName] = useState(""); 
     const [age, setAge] = useState(0); 
     const [isEmployed, setIsEmployed] = useState(false);
+    cont [Student , setIsStudent ] = useState()
 
     const updateName = () => {
         setName("Iraguha");
     };
+    const UpdateStudent = ()=>{
+setIsStudent("Still leraning")
+    }
 
     const increment = () => {
         setAge(age + 1);
@@ -25,6 +29,8 @@ const MyFunc = () => {
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+            <p onClick={UpdateStudent}>Set if is student</p>
+            <button>Set</button>
             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
             <p>Age: {age}</p> 
             <button onClick={increment}>Increment Age</button>
