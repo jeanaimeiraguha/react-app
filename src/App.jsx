a51f628b8968c95ab4b207b6db5bd65384ce3151
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-20 07:17:19 +0200

    React

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyFunc from './component'
+import Message from './message'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <MyFunc/>
+      <Message/>
       
     </>
   )
