9ba148ed63cd4ff5cb98b147fa92bad7ea094700
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-29 13:47:17 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyFunc from './component'
+//import MyFunc from './component'
 //import Message from './message'
 
 function App() {
--- src/component.jsx
+++ src/component.jsx
@@ -4,14 +4,10 @@ const MyFunc = () => {
     const [name, setName] = useState(""); 
     const [age, setAge] = useState(0); 
     const [isEmployed, setIsEmployed] = useState(false);
-    cont [Student , setIsStudent ] = useState()
 
     const updateName = () => {
         setName("Iraguha");
     };
-    const UpdateStudent = ()=>{
-setIsStudent("Still leraning")
-    }
 
     const increment = () => {
         setAge(age + 1);
@@ -29,8 +25,6 @@ setIsStudent("Still leraning")
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
-            <p onClick={UpdateStudent}>Set if is student</p>
-            <button>Set</button>
             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
             <p>Age: {age}</p> 
             <button onClick={increment}>Increment Age</button>
